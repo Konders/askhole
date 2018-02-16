cbb1149dd2e6da6f48e855d42ded71b008b00e64
Olya <30351244+OlVor@users.noreply.github.com>
2018-02-17 00:12:04 +0200

    to save

--- bunitu install/Authorization.Designer.cs
+++ bunitu install/Authorization.Designer.cs
@@ -86,8 +86,7 @@
             this.panel1.Size = new System.Drawing.Size(436, 30);
             this.panel1.TabIndex = 0;
             this.panel1.Tag = "";
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
+           // 
             // panel2
             // 
             this.panel2.Controls.Add(this.bunifuImageButton1);
