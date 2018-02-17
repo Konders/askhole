4972bc6a595a17dfd33753dafea01dd5e3b681dd
Konders <illyashykin5@gmail.com>
2018-02-17 22:20:22 +0200

    Solution refactoring

---
+++ bunitu install/UserControls/ContactList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bunitu_install
+{
+    public partial class ContactList : UserControl
+    {
+        Dictionary<int, string> Contacts = new Dictionary<int, string>();
+        public ContactList()
+        {
+            InitializeComponent();
+            Contacts.Add(0, "Illya");
+        }
+        
+        private void ContactList_MouseEnter(object sender, EventArgs e)
+        {
+            List_Panel.VerticalScroll.Value = List_Panel.VerticalScroll.Maximum;
+        }
+    }
+}
