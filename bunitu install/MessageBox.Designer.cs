9217dc236007d95504c4325fa3f3e31a890d324f
Konders <illyashykin5@gmail.com>
2018-02-17 18:18:07 +0200

    merge

--- bunitu install/MessageBox.Designer.cs
+++ bunitu install/MessageBox.Designer.cs
@@ -29,25 +29,41 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Location = new System.Drawing.Point(15, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(375, 59);
+            this.label1.Size = new System.Drawing.Size(419, 87);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label2.Location = new System.Drawing.Point(371, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "6:13 PM";
             // 
             // MessageBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(228)))), ((int)(((byte)(241)))));
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "MessageBox";
-            this.Size = new System.Drawing.Size(437, 109);
+            this.Size = new System.Drawing.Size(450, 109);
+            this.Resize += new System.EventHandler(this.MessageBox_Resize);
             this.ResumeLayout(false);
 
         }
@@ -55,5 +71,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- bunitu install/MessageBox.cs
+++ bunitu install/MessageBox.cs
@@ -16,5 +16,40 @@ namespace bunitu_install
         {
             InitializeComponent();
         }
+        public enum MessageType
+        {
+            In,
+            Out
+        }
+        public MessageBox(string text,MessageType mt)
+        {
+            InitializeComponent();
+            label1.Text = text;
+            AdjustHeight();
+            if(mt == MessageType.In)
+            {
+                this.BackColor = Color.FromArgb(248, 248, 248);
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(147, 228, 241);
+            }
+        }
+
+        void AdjustHeight()
+        {
+            Size maxSize = new Size(450, int.MaxValue);
+            Graphics g = CreateGraphics();
+            SizeF size = g.MeasureString(label1.Text, label1.Font, label1.Width);
+
+            label1.Height = int.Parse(Math.Round(size.Height + 2, 0).ToString());
+            label2.Top = label1.Bottom;
+            this.Height = label1.Bottom + 10;
+        }
+
+        private void MessageBox_Resize(object sender, EventArgs e)
+        {
+            AdjustHeight();
+        }
     }
 }
