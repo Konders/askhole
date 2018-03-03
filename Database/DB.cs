b77f2da0ec1357cd0359c90674b60157c4b006c1
Olya <30351244+OlVor@users.noreply.github.com>
2018-03-03 14:09:21 +0200

    Refactoring DB

--- Database/DB.cs
+++ Database/DB.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using AskholeLib;
 
 namespace Database
 {
-
     public class DB
     {
         static public SqlConnection cn;
         static public SqlCommand cmd;
         static public Lib.User user = new Lib.User { username = "user" };
         static public Lib.User reciever = new Lib.User { username = "Ivan" };
-        // static public string UserName { get; set; } // поточний користувач
-        //  static public string RecieverName { get; set; } // отримувач
 
         public DB()
         {
@@ -30,19 +23,16 @@ namespace Database
         }
 
         /// <summary>
-        /// Вхід в систему
+        /// Активація та виконання запитів в базі даних
         /// </summary>
-        /// <param name="email">email</param>
-        /// <param name="password">пароль</param>
+        /// <param name="str">запит</param>
         /// <returns></returns>
-        static public bool SignIn(string email, string password)
+        static private bool DBWork(string query)
         {
             try
             {
                 cn.Open(); // запит до бд
-                StringBuilder str = new StringBuilder("exec SignIn '" + email + "', '" +
-                                                        password + "'");
-                cmd.CommandText = Convert.ToString(str);
+                cmd.CommandText = Convert.ToString(query);
                 cmd.ExecuteNonQuery();
                 cn.Close();
                 return true;
@@ -55,25 +45,28 @@ namespace Database
         }
 
         /// <summary>
+        /// Вхід в систему
+        /// </summary>
+        /// <param name="email">email</param>
+        /// <param name="password">пароль</param>
+        /// <returns></returns>
+        static public bool SignIn(string email, string password)
+        {
+            StringBuilder query = new StringBuilder("exec SignIn '" + email + "', '" +
+                                                       password + "'");
+            return DBWork(query.ToString());
+        }
+
+        /// <summary>
         /// Добавлення текстового повідомлення до БД
         /// </summary>
         /// <param name="text">текст повідомлення</param>
         /// <param name="time">час</param>
         static public void AddMessageText(string text, DateTime time)
         {
-            try
-            {
-                cn.Open(); // запит до бд
-                StringBuilder str = new StringBuilder("exec AddingMessage  '" + user.username + "', '" +
-                                       reciever.username + "', '" + text + "', '" + time + "'");
-                cmd.CommandText = Convert.ToString(str);
-                cmd.ExecuteNonQuery();
-                cn.Close();
-            }
-            catch (SqlException ex)
-            {
-                cn.Close();
-            }
+            StringBuilder query = new StringBuilder("exec AddingMessage  '" + user.username + "', '" +
+                                      reciever.username + "', '" + text + "', '" + time + "'");
+            DBWork(query.ToString());
         }
 
         /// <summary>
@@ -86,21 +79,9 @@ namespace Database
         /// <returns></returns>
         static public bool NewUser(string userName, string password, string email, DateTime birthDay)
         {
-            try
-            {
-                cn.Open();
-                StringBuilder comand = new StringBuilder("exec AddUser '" + userName + "', '" +
-                              password + "', '" + email + "', '" + birthDay + "'");
-                cmd.CommandText = Convert.ToString(comand);
-                cmd.ExecuteNonQuery();
-                cn.Close();
-                return true;
-            }
-            catch (SqlException ex)
-            {
-                cn.Close();
-                return false;
-            }
+            StringBuilder query = new StringBuilder("exec AddUser '" + userName + "', '" +
+                             password + "', '" + email + "', '" + birthDay + "'");
+            return DBWork(query.ToString());
         }
 
         /// <summary>
@@ -185,52 +166,21 @@ namespace Database
         /// <param name="date"></param>
         static public void ChangeBirthDay(DateTime date)
         {
-            try
-            {
-                cn.Open();
-                StringBuilder comand = new StringBuilder("exec ChangeBirthDate '" + user.id + "','" + date + "'");
-                cmd.CommandText = Convert.ToString(comand);
-                cmd.ExecuteNonQuery();
-                cn.Close();
-            }
-            catch (SqlException ex)
-            {
-                cn.Close();
-            }
+            StringBuilder query = new StringBuilder("exec ChangeBirthDate '" + user.id + "','" + date + "'");
+            DBWork(query.ToString());
         }
 
         static public void ChangeName(string name)
         {
-            try
-            {
-                cn.Open();
-                StringBuilder comand = new StringBuilder("exec ChangeName " + user.id + ",'" + name + "'");
-                cmd.CommandText = Convert.ToString(comand);
-                cmd.ExecuteNonQuery();
-                cn.Close();
-            }
-            catch (SqlException ex)
-            {
-                cn.Close();
-            }
+            StringBuilder query = new StringBuilder("exec ChangeName " + user.id + ",'" + name + "'");
+            DBWork(query.ToString());
         }
+
         static public bool ResetPassword(string username, string email, string newPassword)
         {
-            try
-            {
-                cn.Open();
-                StringBuilder str = new StringBuilder("exec ToResetPassword '" + username + "', '" +
-                                                        email + "', '" + newPassword + "'");
-                cmd.CommandText = Convert.ToString(str);
-                cmd.ExecuteNonQuery();
-                cn.Close();
-                return true;
-            }
-            catch (SqlException ex)
-            {
-                cn.Close();
-                return false;
-            }
+            StringBuilder query = new StringBuilder("exec ToResetPassword '" + username + "', '" +
+                                                       email + "', '" + newPassword + "'");
+            return DBWork(query.ToString());
         }
     }
 }
